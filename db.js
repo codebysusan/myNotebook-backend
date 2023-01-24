c48eef93d7d71db5758fe6ea6eda9628be08bb47
codebysusan <susankhadka708@gmail.com>
2023-01-24 13:42:52 +0530

    Database connection

---
+++ db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017";
+
+
+const connectToMongo = () =>{
+    mongoose.set("strictQuery", false);
+    mongoose.connect(mongoURI,()=>{
+        console.log("Connected to mongoose successfully");
+    });
+}
+
+module.exports = connectToMongo;
--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-const express = require('express');
+// const express = require('express');
+
+const connectToMongo = require('./db');
+
+connectToMongo();
