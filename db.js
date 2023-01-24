7cf815538bc4c273f9c50987b5c3c64472bfeaf8
codebysusan <susankhadka708@gmail.com>
2023-01-24 20:07:16 +0530

    Implemented jwtwebtoken

--- db.js
+++ db.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
 const mongoURI = "mongodb://localhost:27017/mynotebook";
 
-
 const connectToMongo = () =>{
     mongoose.set("strictQuery", false);
     mongoose.connect(mongoURI,()=>{
         console.log("Connected to mongoose successfully");
     });
 }
-
 module.exports = connectToMongo;
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 
 // Available routes
-app.use("/auth", require("./routes/auth"));
+app.use("/auth", require("./routes/auth/createuser"));
 app.use("/notes", require("./routes/notes"));
 
 app.get("/", (req, res) => {
